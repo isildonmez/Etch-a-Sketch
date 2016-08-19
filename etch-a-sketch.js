03f444b3211840d4c905378cf43c508b0e5c56b6
Isil Donmez <isil.donmez@gmail.com>
2016-08-20 00:49:46 +0100

    Finish randomColor, trailer, createGrid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,40 +3,72 @@ $('document').ready(function() {
 	$('.options').on('click', function (){
 		var	number = prompt("Please enter an integer between 1 and 30 for the sketch size.");
 		while (number < 1 || number > 30) {
-		var number = prompt("To start, please enter an integer between 1 and 30.");
+			var number = prompt("To start, please enter an integer between 1 and 30.");
 		};
-		if (number >= 1 && number <= 30) {
-			createGrid();
-		};
+		createGrid(number);
 	});
+	$('#original').on('click', original);
+	$('#rOriginal').on('click', rOriginal);
+	$('#rColor').on('click', randomColor);
+	$('#trailer').on('click', trailer);
+	$('#clear').on('click', clearGrid);
 });
 
-function createGrid() {
+
+function createGrid(number) {
 	var squareSize = 550 / number;
-	$('.container').prepend($('.grid').css({"width": "squareSize", "height" : "squareSize"}))
+	for(var i=0; i<number*number; i++){
+		$('.container').append($('<div class=grid></div>').css({"width":squareSize, "height":squareSize}));
+	};
+};
+
+//TODO
+function clearGrid() {
+	$('.grid').css({"background-color": "#fcfcfc"});
+};
+
+//TODO
+function original() {
+	$('.grid').css({"background-color":"#212121"});
+	var opacity = $('.grid').css("opacity", 0.1);
+	$('.grid').on('mouseenter', function() {
+		while (opacity < 1) {
+			$(this).css("opacity", opacity + 0.1);
+		};
+	});
+};
+
+//TODO
+function rOriginal() {
+	$('.grid').css({"background-color": "#263238"});
+	$('.grid').on('mouseenter', function() {
+		var opacity = $(this).css("opacity");
+		while (opacity >= 0) {
+			$(this).css("opacity", opacity - 0.1);
+		};
+	});
 };
 
 function randomColor() {
-	createGrid();
 	$('.grid').on('mouseenter', function () {
-		var c1 = #7bb3ff;
-		var c2 = #e86af0;
-		var c3 = #9e379f;
-		var c4 = #493267;
-		var c5 = #373854;
-		var colorNumber = Math.floor(Math.random()*5 + 1);
-		var color = return "var c" + colorNumber;
-		$(this).css({"background-color": "color"});
+		var colors = ["#7bb3ff", "#e86af0", "#9e379f", "#493267", "#373854"];
+		var colorNumber = Math.floor(Math.random()*5);
+		var color = colors[colorNumber];
+		$(this).css({"background-color": color});
 	});
 };
 
-$('.roriginal').on('click',function() {
-	$('.grid').css({"background-color", "#011f4b"});
-	$('.grid').on('mouseenter', function() {
-		var opacity = $(this).css("opacity");
-		while (opacity != 0) {
-			$(this).css({"opacity", opacity - 0.1});
-		};
+function trailer() {
+	$('.grid').css({"background-color": "#9E9E9E"});
+	$('.grid').hover(function(){
+		$(this).css({"opacity":0});
+	}, function() {
+		$(this).fadeTo(1000, 1);
 	});
-		};
-});
+};
+
+
+
+
+
+
